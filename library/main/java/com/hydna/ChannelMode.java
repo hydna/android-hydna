5e2afac7499bac7ccf6ab01597ad74b6b2078087
Johan Dahlberg <dahlberg.johan@gmail.com>
2014-04-22 22:38:59 +0200

    Added missing ChannelMode

---
+++ library/main/java/com/hydna/ChannelMode.java
@@ -0,0 +1,12 @@
+package com.hydna;
+
+public class ChannelMode {
+    public static final int LISTEN = 0x00;
+    public static final int READ = 0x01;
+    public static final int WRITE = 0x02;
+    public static final int READWRITE = 0x03;
+    public static final int EMIT = 0x04;
+    public static final int READEMIT = 0x05;
+    public static final int WRITEEMIT = 0x06;
+    public static final int READWRITEEMIT = 0x07;
+}
